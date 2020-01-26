cbb7f55d6c3f06d293092ed6b6b78e7729d982e5
mahalakshmi525 <60012171+mahalakshmi525@users.noreply.github.com>
2020-01-26 22:57:51 +0530

    Add files via upload

---
+++ java/java 1/addition/src/com/cts/training/addition/Addition.java
@@ -0,0 +1,14 @@
+package com.cts.training.addition;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10;
+		int b =15;
+		int c = a+b;
+		System.out.println("sum is :" + c);
+		
+	}
+
+}
---
+++ java/java 1/personaldetails/src/com/cts/training/personaldetails/Personal.java
@@ -0,0 +1,37 @@
+package com.cts.training.personaldetails;
+
+import java.util.Scanner;
+
+public class Personal {
+
+	public static void main(String[] args) {
+	
+	     
+		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter your name :");
+		String  name = sc.next();
+		System.out.println("enter your age :");
+		int age = sc.nextInt();
+		/*int n ;
+		@SuppressWarnings("unused")
+		String ch=sc.next();*/
+	   // int num = sc.nextInt();
+		/*try {
+		if(name=="ch") {*/
+		System.out.println("your name is :" +name);
+		/*}/*catch(Exception e)
+		 {
+			System.out.println("enter correct name  :");
+		}
+		try {*/
+		/* if(age==num) {*/
+		  System.out.println("your age is :" +age);
+		  /*}catch(Exception e)
+	      {
+		System.out.println("enter correct age :" );
+	     }*/
+}
+	}
+
