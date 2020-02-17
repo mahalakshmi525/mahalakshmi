c68d59e6dd1f9bd6ba6b1974cdf86139a71b5e3e
mahalakshmi525 <60012171+mahalakshmi525@users.noreply.github.com>
2020-02-17 17:02:45 +0530

    Add files via upload

---
+++ spring boot/bootapp/src/main/java/com/example/training/bootapp/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.training.bootapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+      @GetMapping("/test")
+      public ResponseEntity<String> test(){
+    	  ResponseEntity<String> response = 
+    			  new ResponseEntity<String>("this is the test data-added after dev tools", HttpStatus.OK);
+    	  return response;
+      }
+}
